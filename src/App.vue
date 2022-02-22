0fb129f734ce244deb9175af5581b91b954683af
rustin <1543153730@qq.com>
2022-02-22 20:32:10 +0800

    完善aside切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@ import { provide, ref } from 'vue';
 export default {
     name: 'App',
     setup() {
-        const sidebarVisible = ref(false);
+        const width = document.documentElement.clientWidth;
+        const sidebarVisible = ref(width <= 500 ? false : true);
         provide('sidebarVisible', sidebarVisible);
     },
 };
