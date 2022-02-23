9bc103d5d22b33e9b6be03066c76e0facd53fb5a
rustin <1543153730@qq.com>
2022-02-23 21:43:07 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,27 @@
 <template>
     <div>Button</div>
+    <h1>示例1</h1>
+    <div>
+        <Button>你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
+    </div>
 </template>
+
+<script>
+import Button from '../libs/Button.vue';
+export default {
+    components: {
+        Button,
+    },
+    setup() {
+        const onClick = () => {
+            alert('你好');
+        };
+        return {
+            onClick,
+        };
+    },
+};
+</script>
---
+++ src/libs/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button class="ui-button" :class="`theme-${theme}`">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    props: {
+        theme: {
+            type: String,
+            default: 'button',
+        },
+    },
+    setup(props, context) {
+        const onClick = () => {
+            context.emit('click');
+        };
+        return {
+            onClick,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
