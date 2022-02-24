8efcf9fe5557016b60172496d84c85c2af0cad95
rustin <1543153730@qq.com>
2022-02-24 19:35:35 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,13 @@
 <template>
     <div>Dialog</div>
+    <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../libs/Dialog.vue';
+export default {
+    components: {
+        Dialog,
+    },
+};
+</script>
---
+++ src/libs/Dialog.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行字</p>
+            <p>第二行字</p>
+        </main>
+        <footer>
+            <button>OK</button>
+            <button>Cancel</button>
+        </footer>
+    </div>
+</template>
