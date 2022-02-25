cd157508b082428fe2cb5793321fe72707cab2b2
rustin <1543153730@qq.com>
2022-02-25 20:35:56 +0800

    使用CSS实现切换显示

--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,42 +1,54 @@
 <template>
-    <div>
-        <div class="gulu-tabs">
-  <div class="gulu-tabs-nav">
-    <div class="gulu-tabs-nav-item"
-    @click="select(t)"
-     v-for="(t,index) in titles" 
-     :key="index"
-     :class="{selected:t===selected}"
-     >{{t}}</div>
-  </div>
-  <div class="gulu-tabs-content">
-    <component class="gulu-tabs-content-item" :is="current"/>
-  </div>
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div
+                class="gulu-tabs-nav-item"
+                v-for="(t, index) in titles"
+                @click="select(t)"
+                :class="{ selected: t === selected }"
+                :key="index"
+            >
+                {{ t }}
+            </div>
+        </div>
+        <div class="gulu-tabs-content">
+            <component
+                class="gulu-tabs-content-item"
+                :class="{ selected: c.props.title === selected }"
+                v-for="(c, index) in defaults"
+                :is="c"
+                :key="index"
+            />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { computed } from '@vue/runtime-core';
-import Tab from '../libs/Tab.vue';
+import Tab from './Tab.vue';
+import { computed } from 'vue';
 export default {
-    props:{
-        selected:{
-            type:String,
-        }
+    props: {
+        selected: {
+            type: String,
+        },
     },
     setup(props, context) {
         const defaults = context.slots.default();
-
         defaults.forEach((tag) => {
             if (tag.type !== Tab) {
-                throw new Error('Tabs only accepts Tab as children');
+                throw new Error('Tabs 子标签必须是 Tab');
             }
         });
-        const current =computed(()=>{return defaults.filter((tag) => tag.props.title === props.selected)[0];}) 
+        const current = computed(() => {
+            console.log('重新 return');
+            return defaults.filter((tag) => {
+                return tag.props.title === props.selected;
+            })[0];
+        });
         const titles = defaults.map((tag) => {
             return tag.props.title;
         });
-        const select = (title:string) => {
+        const select = (title: string) => {
             context.emit('update:selected', title);
         };
         return {
@@ -53,25 +65,38 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .gulu-tabs {
-  &-nav {
-    display: flex;
-    color: $color;
-    border-bottom: 1px solid $border-color;
-    &-item {
-      padding: 8px 0;
-      margin: 0 16px;
-      cursor: pointer;
-      &:first-child {
-        margin-left: 0;
-      }
-      &.selected {
-        color: $blue;
-      }
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
     }
-  }
-  &-content {
-    padding: 8px 0;
-  }
+
+    &-content {
+        padding: 8px 0;
+
+        &-item {
+            display: none;
+
+            &.selected {
+                display: block;
+            }
+        }
+    }
 }
 </style>
