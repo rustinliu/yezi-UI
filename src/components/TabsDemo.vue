d7c2f659dc024d2d0591bf6e59124f81dfd791f9
rustin <1543153730@qq.com>
2022-02-26 20:00:05 +0800

    继续优化Demo

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,25 +1,18 @@
 <template>
-    <div>Tabs</div>
-    <h1>示例一</h1>
-    <Tabs v-model:selected="x">
-        <Tab title="导航一">内容一</Tab>
-        <Tab title="导航二">内容二</Tab>
-    </Tabs>
+    <h1>Tabs 示例</h1>
+    <Demo :component="Tabs1Demo" />
 </template>
 
 <script lang="ts">
-import Tabs from '../libs/Tabs.vue';
-import Tab from '../libs/Tab.vue';
-import { ref } from 'vue';
+import Demo from './Demo.vue';
+import Tabs1Demo from './Tabs1.demo.vue';
 export default {
     components: {
-        Tabs,
-        Tab,
+        Demo,
     },
     setup() {
-        const x = ref('导航一');
         return {
-            x,
+            Tabs1Demo,
         };
     },
 };
