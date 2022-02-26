76661ada16596581f9f2cec6b3cd49215767530b
rustin <1543153730@qq.com>
2022-02-26 15:47:08 +0800

    添加更多路由

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,9 @@ import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
-
+import Intro from './views/Intro.vue';
+import GetStarted from './views/GetStarted.vue';
+import Install from './views/Install.vue';
 const history = createWebHashHistory();
 export const router = createRouter({
     history,
@@ -19,6 +21,9 @@ export const router = createRouter({
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
                 { path: 'tabs', component: TabsDemo },
+                { path: 'intro', component: Intro },
+                { path: 'get-started', component: GetStarted },
+                { path: 'install', component: Install },
             ],
         },
     ],
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,6 +3,12 @@
         <Topnav toggleMenuButtonVisible class="nav" />
         <div class="content">
             <aside v-if="sidebarVisible">
+                <h2>文档</h2>
+                <ol>
+                    <li><router-link to="/doc/intro">介绍</router-link></li>
+                    <li><router-link to="/doc/install">安装</router-link></li>
+                    <li><router-link to="/doc/get-started">开始使用</router-link></li>
+                </ol>
                 <h2>组件列表</h2>
                 <ol>
                     <li>
@@ -43,6 +49,9 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.router-link-active {
+    text-decoration: underline;
+}
 .layout {
     display: flex;
     flex-direction: column;
